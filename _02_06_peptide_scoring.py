969cbbaaa2e702af0e65bb7092510160f29405d4
Oleg Kainov <oleg.kainov@intel.com>
2015-11-26 22:14:43 +0300

    Refactor

--- _02_06_peptide_scoring.py
+++ _02_06_peptide_scoring.py
@@ -1,29 +1,35 @@
 from _02_04_cyclopeptide_gen import cyclopeptide
 
+
+def score(peptide, perfect_spectrum):
+    actual_spectrum = cyclopeptide(peptide, table)
+    result_score = 0
+    i1 = 0
+    i2 = 0
+    actual_spectrum = map(int, actual_spectrum)
+    perfect_spectrum = map(int, perfect_spectrum)
+    while i1 < len(actual_spectrum) and i2 < len(perfect_spectrum):
+        if actual_spectrum[i1] == perfect_spectrum[i2]:
+            result_score += 1
+            i1 += 1
+            i2 += 1
+        elif actual_spectrum[i1] > perfect_spectrum[i2]:
+            i2 += 1
+        elif actual_spectrum[i1] < perfect_spectrum[i2]:
+            i1 += 1
+    return result_score
+
+
 if __name__ == '__main__':
     with open('in.txt', 'r') as f:
         text = f.readline().strip()
-        expected_spectrum = f.readline().split()
+        perfect_spectrum = f.readline().split()
     with open('peptide_table.txt', 'r') as f:
         table = {}
         for line in f.readlines():
             letter, w = line.split()
             w = int(w)
             table[letter] = w
-    perfect_spectrum = cyclopeptide(text, table)
-    score = 0
-    i1 = 0
-    i2 = 0
-    perfect_spectrum = map(int, perfect_spectrum)
-    expected_spectrum = map(int, expected_spectrum)
-    while i1 < len(perfect_spectrum) and i2 < len(expected_spectrum):
-        if perfect_spectrum[i1] == expected_spectrum[i2]:
-            score += 1
-            i1 += 1
-            i2 += 1
-        elif perfect_spectrum[i1] > expected_spectrum[i2]:
-            i2 += 1
-        elif perfect_spectrum[i1] < expected_spectrum[i2]:
-            i1 += 1
+    n_score = score(text, perfect_spectrum)
     with open('out.txt', 'w') as f:
-        f.write(str(score))
+        f.write(str(n_score))
